d5ea53477d84083e86c14115bd2588c91087005f
angeella <a.andreella8@gmail.com>
2020-07-23 10:57:55 +0200

    update lambda OPt

--- R/lambdaOpt.R
+++ R/lambdaOpt.R
@@ -16,7 +16,7 @@ lambdaOpt <- function(pvalues, family, ct = c(0,1), alpha, delta = 0){
   family_set <- c("simes", "finner", "beta", "higher.criticism")
   
   family <- match.arg(tolower(family), family_set)
-  if(is.unsorted(pvalues[1,])){pvalues = rowSortC(pvalues)}
+ # if(is.unsorted(pvalues[,1])){pvalues = rowSortC(pvalues)}
   #implement set of threshold
   lambdaE <- lambdaCalibrate(X = pvalues, alpha = alpha, delta = delta, family = family)
   
