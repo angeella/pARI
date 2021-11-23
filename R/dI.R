a472302c9db30596d15a1ee66fd31b734d784f4b
angeella <a.andreella8@gmail.com>
2021-11-23 17:08:02 +0100

    update dI

--- R/dI.R
+++ R/dI.R
@@ -24,9 +24,9 @@ dI <- function(ix, cv, pvalues, iterative = FALSE, approx = TRUE, ncomb = 100, .
   if(!iterative){
     family <- delta <- alpha <- NULL
   }
-  if(iterative & !(exists("family", mode = "character") & exists("delta") & exists("alpha"))){
-    stop("Please specify the family of confidence bounds, delta and alpha levels if you want to use the iterative approach")
-  }
+ # if(iterative & !(exists("family", mode = "character") & exists("delta") & exists("alpha"))){
+ #   stop("Please specify the family of confidence bounds, delta and alpha levels if you want to use the iterative approach")
+ # }
   
   d <- permDiscoveries(ix = ix, cv = cv, praw = pvalues[,1])
   if(iterative){
