eb424c3cf490837911186f3fefd6877c9e5bb0aa
angeella <a.andreella8@gmail.com>
2021-11-23 12:06:00 +0100

    update seed simulateData

--- R/simulateData.R
+++ R/simulateData.R
@@ -15,14 +15,15 @@
 #' @importFrom stats power.t.test
 
 simulateData <- function(pi0,m,n, rho, seed = NULL, power = 0.8, alpha = 0.05){
-  if(is.null(seed)){set.seed(sample.int(1e5, 1))}
   if(is.null(n) & !is.null(power)){stop("Please insert sample size n")}
   m0 = round(m*pi0)
   m1 = round(m -m0)
+  if(is.null(seed)){set.seed(sample.int(1e5, 1))}else{set.seed(seed)}
   pwo <- power.t.test(power = power, n=n, sig.level = alpha, type = "one.sample", alternative = "two.sided", sd = 1)
   diff_mean<-pwo$delta
   #diff_mean <-rgamma(m1, shape = pwo$delta, scale = 1)
   #diff_mean <- rnorm(m1, mean = pwo$delta, sd = 1000)
+  if(is.null(seed)){set.seed(sample.int(1e5, 1))}else{set.seed(seed)}
   eps <- sqrt(1-rho)*matrix(rnorm(m*n), ncol=m)  + sqrt(rho)*matrix(rep(rnorm(n),m), ncol=m) 
   #mu <- c(diff_mean, rep(0, m0))
   mu <- c(rep(diff_mean,m1), rep(0, m0))
